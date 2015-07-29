53559dd97511d70eebc69b4c1aed487e60cb4d2f
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2015-07-29 15:01:56 +0000

    rails 4 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,1 @@
-if Rails::VERSION::MAJOR < 3
-  ActionController::Routing::Routes.draw do |map|
-    map.plugin_route 'searchissues', :controller => "search_issues", :action => 'index', :format => 'json'
-  end
-else
-  match 'searchissues', :controller => "search_issues", :action => 'index', :format => 'json'
-end
+  get 'searchissues', :controller => "search_issues", :action => 'index', :format => 'json'
